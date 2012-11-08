cdd3f0aba4bb18a07e851c6728b096e0c8cbb62c
Suhani N Mehta <suhani@berkeley.edu>
2012-11-07 21:41:43 -0800

    Adding twitter search api and conversion of data

---
+++ writing.php
@@ -0,0 +1,26 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+$CSVstring= $_POST['tweetsString']; 
+
+$my_file = 'Tweets.txt';  //Note: The file permissions for the text file must be 777 i.e. rwxrwxrwx
+
+
+
+//APPENDING TO FILE
+$handle = fopen($my_file, 'a') or die('Cannot open file:  '.$my_file);
+fwrite($handle, $CSVstring);
+fclose($handle);
+
+
+
+//READING FROM FILE
+// $handle = fopen($my_file, 'r') or die('Cannot open file:  '.$my_file);
+// $CSVdata = fread($handle,filesize($my_file));
+// fclose($handle);
+
+
+echo 'Pass CSVdata read from file here';
+
+// echo $CSVstring;
+?>
